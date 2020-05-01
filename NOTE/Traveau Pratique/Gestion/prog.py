a0106056b81ad5d2311d1a4a6c58a17d3d9fc519
PRCInformatique <PRC_CIA1@users.noreply.github.com>
2020-05-01 16:05:46 +0100

    prcinformatique.github.io/NOTE

--- NOTE/Traveau Pratique/Gestion/prog.py
+++ NOTE/Traveau Pratique/Gestion/prog.py
@@ -1,42 +1,29 @@
 #Coding:utf-8
-import pygame
-from pygame.locals import*
 
-pygame.init()
-fenetre = pygame.display.set_mode((876,1200), RESIZABLE)
-fond1 = pygame.image.load("a1.jpg").convert_alpha()
-fond2 = pygame.image.load("a2.png")
-pax = 0
-pay = 0
-position_fond1 = fond1.get_rect()
-fenetre.blit(fond1, position_fond1)
-fenetre.blit(fond2,(pax,pay))
-
-pygame.display.flip()
-
-pygame.key.set_repeat(400, 3)
-continuer = 1
-while continuer:
-	for event in pygame.event.get():
-		if event.type == MOUSEBUTTONDOWN:
-			if event.button == 1:
-				if event.type == MOUSEMOTION:
-					pax = event.pos[0]
-					pay = event.pos[1]
-		if event.type == QUIT:
-			continuer = 0
-		if event.type == KEYDOWN:
-			if event.key == K_DOWN or event.key == K_8:
-				position_fond1 = position_fond1.move(0,10)
-
-			if event.key == K_UP or event.key == K_2:
-				position_fond1 = position_fond1.move(0,-10)
-
-			if event.key == K_RIGHT or event.key == K_4:
-				position_fond1 = position_fond1.move(10,0)
-
-			if event.key == K_LEFT or event.key == K_6:
-				position_fond1 = position_fond1.move(-10,0)
-	fenetre.blit(fond1, position_fond1)
-	fenetre.blit(fond2, (pax,pay))
-	pygame.display.flip()
+class essaie:
+	a = 0
+	def __init__(self,v1,v2):
+		essaie.a += 1
+		self.v1 = v1
+		self.v2 = v2
+	def addition(cls,v1,v2):
+		return v1+v2
+	addition = classmethod(addition)
+	def sustration(v1,v2):
+		return v1-v2
+	sustration = staticmethod(sustration)
+	def calcule(self,signe):
+		if signe == '+':
+			r = essaie.addition(self.v1,self.v2)
+			return r 
+	def __repr__(self):
+		return f"JE suis {self.v1} "
+	def __setattr__(self,nom,valeur):
+		valeur+=valeur
+		object.__setattr__(self,nom,valeur)
+p = essaie(49,53)
+#print (p)
+r = p.calcule('+')
+#p.v2 = 60
+print (p.v2)
+print (r)
